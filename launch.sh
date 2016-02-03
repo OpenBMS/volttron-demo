4bca58be464d6c2f12bbc8cff20813202d872e5d
Elomar Souza <elomar@elomar.me>
2016-02-02 16:43:22 -0800

    Install plug dependencies and agent, and start X server to run selenium

--- launch.sh
+++ launch.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 source /home/volttron/volttron-source/env/bin/activate
+DISPLAY=:1 xvfb-run java -jar ~/.selenium/selenium-server-standalone-2.50.0.jar > selenium.log &
 volttron -l volttron.log -vv&
 
+pip install demo/entities/Plug
+
 volttron-ctl start --name testagent-0.1
+volttron-ctl start --name plug_openbms_entity-0.1
