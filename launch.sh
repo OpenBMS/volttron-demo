5be591b81464a8355134b9375a38691cbf532222
Elomar Souza <elomar@elomar.me>
2016-02-02 23:57:07 -0800

    Add a delay so selenium is ready by the time VOLTTRON starts

--- launch.sh
+++ launch.sh
@@ -4,5 +4,7 @@ source /home/volttron/volttron-source/env/bin/activate
 
 DISPLAY=:1 xvfb-run java -jar .selenium/selenium-server-standalone-2.50.0.jar > selenium.log &
 
+sleep 3
+
 volttron -l volttron.log -vv&
 
