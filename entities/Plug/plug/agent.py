c324a953d0b6662667d2858ca42796b3cf3c7db3
Elomar Souza <elomar@elomar.me>
2016-02-02 18:52:57 -0800

    Setup logging for plug agent, update style to match new core agents

--- entities/Plug/plug/agent.py
+++ entities/Plug/plug/agent.py
@@ -1,8 +1,9 @@
 import sys
 import datetime
+import logging
 import os
 
-from volttron.platform.vip.agent import Agent as BaseAgent, PubSub, Core
+from volttron.platform.vip.agent import Agent, PubSub, Core
 from volttron.platform.agent import utils, matching
 from volttron.platform.messaging import headers as headers_mod
 from zmq.utils import jsonapi
@@ -10,25 +11,20 @@ from zmq.utils import jsonapi
 from boss_client import BossClient
 import settings
 
+utils.setup_logging() 
+_log = logging.getLogger(__name__)
 
-def PlugEntity(config_path, **kwargs):
-    config = utils.load_config(config_path)
-
-    def get_config(name):
-        try:
-            return kwargs.pop(name)
-        except KeyError:
-            return config.get(name, '')
-
-    AGENT_ID = get_config("agent_id")
-    BOSS_DEVICE_NAME = get_config('boss_device_name')
-
+def plug_entity(config_path, **kwargs):
     BOSS_EMAIL = os.environ['BOSS_EMAIL']
     BOSS_PASSWORD = os.environ['BOSS_PASSWORD']
 
-    class Agent(BaseAgent):
+    config = utils.load_config(config_path)
+    AGENT_ID = config.get("agent_id")
+    BOSS_DEVICE_NAME = config.get('boss_device_name')
+
+    class PlugEntity(Agent):
         def __init__(self, **kwargs):
-            super(Agent, self).__init__(**kwargs)
+            super(PlugEntity, self).__init__(**kwargs)
             self.boss_client = BossClient(BOSS_EMAIL, BOSS_PASSWORD, BOSS_DEVICE_NAME)
 
         @Core.periodic(settings.HEARTBEAT_PERIOD)
@@ -42,27 +38,27 @@ def PlugEntity(config_path, **kwargs):
               headers_mod.FROM: AGENT_ID,
               headers_mod.CONTENT_TYPE: headers_mod.CONTENT_TYPE.JSON,
               headers_mod.DATE: datetime.datetime.today().isoformat(),
-              "Location": get_config("location")
+              "Location": config.get("location")
             }
+            _log.debug("publishing heartbeat")
             self.publish_all(data, prefix, headers)
 
         def publish_all(self, data, prefix, headers):
             for item in data.keys():
                 topic = prefix + '/' + item
-                self.vip.pubsub.publish_json('pubsub', topic, headers, str(data[item]))
+                self.vip.pubsub.publish('pubsub', topic, headers, jsonapi.dumps(data[item]))
 
         @PubSub.subscribe('pubsub', settings.TYPE_PREFIX + '/' + AGENT_ID + '/operations/switch')
         def on_switch(self, topic, headers, message, match):
             print 'Plug got\nTopic: {topic}, {headers}, Message: {message}'.format(topic=topic, headers=headers, message=message)
             self.boss_client.switch(jsonapi.loads(message[0]))
   
-    Agent.__name__ = 'PlugEntity'
-    return Agent(**kwargs)
+    return PlugEntity(**kwargs)
 
 
 def main(argv=sys.argv):
     """Main method called by the platform"""
-    utils.vip_main(PlugEntity)
+    utils.vip_main(plug_entity)
 
 if __name__ == '__main__':
     try:
