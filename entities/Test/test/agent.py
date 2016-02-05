8fb892383d117d07153d184fbae669ba48ac5b3e
Elomar Souza <elomar@elomar.me>
2016-02-04 16:07:57 -0800

    Fix dummy test agent

--- entities/Test/test/agent.py
+++ entities/Test/test/agent.py
@@ -1,15 +1,19 @@
+import logging
 import sys
 
 from volttron.platform.vip.agent import Agent, PubSub
 from volttron.platform.agent import utils
 
+utils.setup_logging() 
+_log = logging.getLogger(__name__)
+
 class TestAgent(Agent):
   def __init__(self, config_path, **kwargs):
     super(TestAgent, self).__init__(**kwargs)
 
-    @PubSub.subscribe('pubsub', '')
-    def on_heartbeat_topic(self, peer, sender, bus, topic, headers, message):
-      print "TestAgent got\nTopic: {topic}, {headers}, Message: {message}".format(topic=topic, headers=headers, message=message)
+  @PubSub.subscribe('pubsub', '')
+  def on_heartbeat_topic(self, peer, sender, bus, topic, headers, message):
+    _log.debug("TestAgent got\nTopic: {topic}, {headers}, Message: {message}".format(topic=topic, headers=headers, message=message))
 
 def main(argv=sys.argv):
   '''Main method called by the platform.'''
