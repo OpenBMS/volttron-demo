2944accdd540b1cd7b59ab769c00e262a66f0a06
Elomar Souza <elomar@elomar.me>
2016-02-12 18:37:57 -0800

    Only try to start volttron after starting selenium

--- launch.sh
+++ launch.sh
@@ -4,7 +4,11 @@ source /home/volttron/volttron-source/env/bin/activate
 
 DISPLAY=:1 xvfb-run java -jar .selenium/selenium-server-standalone-2.50.0.jar > selenium.log &
 
-sleep 3
+wget -O /dev/null http://127.0.0.1:4444/wd/hub/status
+while [ $? -ne 0 ]
+do
+  wget -O /dev/null http://127.0.0.1:4444/wd/hub/status
+done
 
 volttron -l volttron.log -vv&
 
